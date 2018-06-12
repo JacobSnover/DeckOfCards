d9e32710c0400b8b12d068965bf5bade7731e940
Jacob R. Snover <snoverjacob@yahoo.com>
2018-06-12 11:23:52 -0400

    First Commit of working proj

---
+++ DeckOfCards/Controllers/HomeController.cs
@@ -0,0 +1,134 @@
+﻿using DeckOfCards.Models;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace DeckOfCards.Controllers
+{
+    public class HomeController : Controller
+    {
+        public static int[] indexes = { 0, 1, 2, 3, 4 };
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        //call the API to get the data for my deck of cards
+        public ActionResult Game()
+        {
+            HttpWebRequest data = WebRequest.CreateHttp("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1");
+
+            HttpWebResponse Response;
+
+            Response = (HttpWebResponse)data.GetResponse();
+
+            StreamReader sr = new StreamReader(Response.GetResponseStream());
+
+            JObject JsonData = JObject.Parse(sr.ReadToEnd());
+
+            Session["deckID"] = JsonData["deck_id"];
+
+            return RedirectToAction("GetCards");
+        }
+        //this makes an API call to get a new deck of cards
+        public ActionResult GetCards()
+        {
+            DeckOfCardsEntities ORM = new DeckOfCardsEntities();
+            Card tempCard;
+
+            NewCard Deck = new NewCard();
+
+            object ID = Session["deckID"];
+            HttpWebRequest data = WebRequest.CreateHttp($"https://deckofcardsapi.com/api/deck/{ID}/draw/?count=52");
+
+            HttpWebResponse Response;
+            Response = (HttpWebResponse)data.GetResponse();
+            StreamReader sr = new StreamReader(Response.GetResponseStream());
+
+            JObject JsonData = JObject.Parse(sr.ReadToEnd());
+            ViewBag.Remain = JsonData;
+            ViewBag.Cards = JsonData["cards"];
+
+            for (int i = 0; i < ViewBag.Cards.Count; i++)
+            {
+                Deck.AddToDeck(new NewCard(ViewBag.Cards[i]["image"], ViewBag.Cards[i]["value"], ViewBag.Cards[i]["suit"], ViewBag.Cards[i]["code"]));
+            }
+
+            //after grabbing a new deck of cards, I then add the data to my DB
+            //as a deck of cards, so that I can call on my server instead of the API
+            for (int i = 0; i < ViewBag.Cards.Count; i++)
+            {
+                tempCard = new Card();
+                tempCard.CardImage = ViewBag.Cards[i]["image"];
+                tempCard.CardValue = ViewBag.Cards[i]["value"];
+                tempCard.CardSuit = ViewBag.Cards[i]["suit"];
+                tempCard.CardCode = ViewBag.Cards[i]["code"];
+                ORM.Card.Add(tempCard);
+                ORM.SaveChanges();
+            }
+
+            ViewBag.Indexes = indexes;
+            Session["Deck"] = Deck;
+            ViewBag.Deck = Deck.DeckOfCards;
+            ViewBag.Keep = null;
+            ViewBag.Count = 5;
+
+            return View();
+        }
+
+        public ActionResult DrawCards(string[] data)
+        {
+            NewCard Deck = (NewCard)Session["Deck"];
+            int drawCount = 0;
+            List<NewCard> Draw = new List<NewCard>();
+            List<object> Keepers = new List<object>();
+
+            if (data != null)
+            {
+                foreach (var item in data)
+                {
+                    Keepers.Add(Deck.DeckOfCards[Convert.ToInt32(item)].CardImage);
+                }
+                Deck.count += data.Length;
+                drawCount = data.Length;
+            }
+
+            for (int i = 0; i < 5 - drawCount; i++)
+            {
+                Deck.count++;
+                if (Deck.count > 51)
+                {
+                    Deck.count = 4;
+                    return RedirectToAction("Game");
+                }
+                Draw.Add(Deck.DeckOfCards[Deck.count]);
+            }
+
+            ViewBag.Indexes = indexes;
+            ViewBag.Keep = Keepers;
+            ViewBag.Deck = Draw;
+            ViewBag.Count = 5 - drawCount;
+
+            return View("GetCards");
+        }
+    }
+}
---
+++ DeckOfCards/Models/NewCard.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DeckOfCards.Models
+{
+    public class NewCard
+    {
+        private object cardImage;
+        private object cardValue;
+        private object cardSuit;
+        private object cardCode;
+        public int count = 4;
+        private int index = 0;
+        private NewCard[] deckOfCards;
+
+        public object CardImage { get => cardImage; set => cardImage = value; }
+        public object CardValue { get => cardValue; set => cardValue = value; }
+        public object CardSuit { get => cardSuit; set => cardSuit = value; }
+        public object CardCode { get => cardCode; set => cardCode = value; }
+        public NewCard[] DeckOfCards { get => deckOfCards; set => deckOfCards = value; }
+
+        public NewCard(object image, object val, object suit, object code)
+        {
+            CardImage = image;
+            CardValue = val;
+            CardSuit = suit;
+            CardCode = code;
+        }
+
+        public NewCard()
+        {
+            deckOfCards = new NewCard[52];
+        }
+
+        public void AddToDeck(NewCard data)
+        {
+            DeckOfCards[index] = data;
+            index++;
+        }
+    }
+}
